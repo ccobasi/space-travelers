1fe21d65da1a8a80194f746fffa97bd6c57ec58f
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-09-08 21:57:16 +0100

    implement reserve badge

--- src/components/RocketDisplay.js
+++ src/components/RocketDisplay.js
@@ -33,7 +33,10 @@ const Rocket = (props) => {
       <img className="Rocket-Img" src={flickrImages} alt={rocketName} />
       <div className="Rocket-Info">
         <h3 className="Rocket-Title">{rocketName}</h3>
-        <p className="Rocket-Text">{description}</p>
+        <p className="Rocket-Text">
+          <ReserveBadge reserved={reserved} />
+          {description}
+        </p>
         <ReserveButton
           reserved={reserved}
           bookRocket={booked}
@@ -53,6 +56,14 @@ const ReserveButton = (props) => {
   return <button className="Rocket-Button" type="button" onClick={bookRocket}>Reserve Rocket</button>;
 };
 
+const ReserveBadge = (props) => {
+  const { reserved } = props;
+  if (reserved) {
+    return <span><button type="button" className="Rocket-Reserve">Reserved</button></span>;
+  }
+  return <span />;
+};
+
 Rocket.propTypes = {
   flickrImages: PropTypes.any,
   rocketName: PropTypes.any,
@@ -63,6 +74,10 @@ Rocket.propTypes = {
 ReserveButton.propTypes = {
   reserved: PropTypes.any,
   bookRocket: PropTypes.any,
+}.isRequired;
+
+ReserveBadge.propTypes = {
+  reserved: PropTypes.any,
 }.isRequired;
 
 export default Rocket;
