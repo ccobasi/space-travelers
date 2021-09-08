e782bdafb342ed97507c17591a74f2b937c146d2
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-09-08 18:42:17 +0100

    add reserve button

--- src/components/RocketDisplay.js
+++ src/components/RocketDisplay.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import '../index.css';
+import { bookRocket } from '../redux/rockets/rockets';
 
 const Rocket = (props) => {
   const { rocket } = props;
+  const dispatch = useDispatch();
 
   const {
-    id, rocketName, description, flickrImages,
+    id, rocketName, description, flickrImages, reserved,
   } = rocket;
+
+  const booked = () => {
+    dispatch(
+      bookRocket({
+        id,
+      }),
+    );
+  };
 
   return (
     <div id={id} className="Rocket-Container">
@@ -15,16 +26,34 @@ const Rocket = (props) => {
       <div className="Rocket-Info">
         <h3 className="Rocket-Title">{rocketName}</h3>
         <p className="Rocket-Text">{description}</p>
-        <button className="Rocket-Button" type="button">Reserve Rocket</button>
+        <ReserveButton
+          reserved={reserved}
+          bookRocket={booked}
+        />
       </div>
     </div>
   );
+};
+
+const ReserveButton = (props) => {
+  const { reserved, bookRocket } = props;
+
+  if (reserved) {
+    return <button className="Rocket-Button" type="button">Cancel Reservations</button>;
+  }
+  return <button className="Rocket-Button" type="button" onClick={bookRocket}>Reserve Rocket</button>;
 };
 
 Rocket.propTypes = {
   flickrImages: PropTypes.any,
   rocketName: PropTypes.any,
   description: PropTypes.any,
+  reserved: PropTypes.any,
+}.isRequired;
+
+ReserveButton.propTypes = {
+  reserved: PropTypes.any,
+  bookRocket: PropTypes.any,
 }.isRequired;
 
 export default Rocket;
