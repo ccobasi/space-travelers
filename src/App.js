17cb15f47e524db42d07af21fe7ac487dadf3849
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-09-09 23:33:33 +0100

    remove commented out code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/header';
 import Rockets from './components/Rockets';
-// import Profile from './components/Profile/Profile';
 import Profile from './components/Profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
